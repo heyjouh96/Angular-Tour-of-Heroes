bf1b32117c95e8ec60bb269d9ed99e9b050c449c
Jonathan Araujo <jnh.araujo@outlook.com>
2019-02-16 17:22:58 -0200

    searcing heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,12 +24,12 @@ export class HeroesComponent implements OnInit {
   }
 
   add(name: string): void {
-    name = name.trim();
-    if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
-      .subscribe(hero => {
-        this.heroes.push(hero);
-      })
+    // name = name.trim();
+    // if (!name) { return; }
+    // this.heroService.addHero({ name } as Hero)
+    //   .subscribe(hero => {
+    //     this.heroes.push(hero);
+    //   })
   }
 
   delete(hero: Hero): void {
